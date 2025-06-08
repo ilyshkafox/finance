ff4152a18ed4cd29a098c4be187eebb762a1b2e7
ilyshka <ilya_moslov@mail.ru>
2025-06-08 16:46:47 +0300

    INIT

---
+++ src/test/java/ru/ilyshka/finance/FinanceApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.ilyshka.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/ru/ilyshka/finance/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package ru.ilyshka.finance;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
